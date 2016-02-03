b7c480cb040c36151fd4f2d270d77876e5e0cde9
Dan Molik <dan@d3fy.net>
2016-02-03 13:13:05 -0500

    Drop bzero in favor of memset

--- src/fping.c
+++ src/fping.c
@@ -1666,7 +1666,7 @@ void add_name(char *name) /* {{{ */
 	char              addrbuf[256];
 
 	/* getaddrinfo */
-	bzero(&hints, sizeof(struct addrinfo));
+	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_flags = 0;
 	hints.ai_socktype = SOCK_RAW;
 #ifndef IPV6
